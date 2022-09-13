117f3b4ae4b2dae2d1e9509dbd14bb1549e42a92
Noz4416 <no.z4419@gmail.com>
2022-09-13 12:38:20 +0000

    change12

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -31,31 +31,42 @@ class Recipe < ApplicationRecord
 
 # 栄養素の計算式
   def calculate(column, recipe)
-    materials_sum = 0
-    convert_amount = 0
-    materials_amount = 0
-
+    # p "==========================================="
+    # p "start"
+    return_value = 0
     recipe.ingredients.each do |ing|
-      unless ing.material_id
-      next
-      end
-        materials_sum += ing.material["#{column}"]
-# 単位による条件分岐
+      next unless ing.material_id
+      materials_sum = ing.material["#{column}"]
+      # p "column: #{column}"
+      # p "name: #{ing.material.name}"
+      # p "materials_sum: #{materials_sum}"
+      # 単位による条件分岐
+      materials_amount = 0
+      convert_amount = 0
       if ing.unit == ('g'||'cc'||'ml')
-        materials_amount += ing.amount.to_f
+        materials_amount = ing.amount.to_f
       elsif ing.unit == 'kg'
-        materials_amount += (ing.amount.to_f) * 1000
+        materials_amount = (ing.amount.to_f) * 1000
       elsif ing.unit == 'mg'
-        materials_amount += (ing.amount.to_f) * 0.001
+        materials_amount = (ing.amount.to_f) * 0.001
       else
         unit = Unit.find_by(material_id: ing.material_id, unit: ing.unit)
         if unit
-          convert_amount += (unit.g * ing.amount.to_f)
+          convert_amount = (unit.g * ing.amount.to_f)
+          # p "convert_amount: #{convert_amount}"
         else
         end
       end
+
+      # p "total materials_sum: #{materials_sum}"
+      # p "total convert_amount: #{convert_amount}"
+      ret = ((materials_sum / 100) * (convert_amount + materials_amount))
+      # p "ret: #{ret}"
+      return_value += ret
     end
-    ((materials_sum / 100) * (convert_amount + materials_amount)).floor(1)
+    # p "return_value: #{return_value}"
+    # p "==========================================="
+    return_value.floor(1)
   end
 
 
